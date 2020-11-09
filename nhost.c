452c955cf07c111547fd3b5e459f4e3518d76557
Asher Lieber <asher.lieber@madeinspace.us>
2020-11-08 20:50:16 -0500

    added nhost.c

---
+++ nhost.c
@@ -0,0 +1,20 @@
+/* two ideas - persistent connection - once it's closed
+ *   memory is freed
+ *   client needs to establish connection and keep it open
+ *   (no authentication required)?
+ * 
+ *   no persistent connections - based on IP address and UID
+ *   THIS 
+ */
+#include <stdio.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+/* stores connecters by ip and port */
+struct requester_cont{
+};
+
+void* accept_conn(void* x){
+}
