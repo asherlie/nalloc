21759a3410bdb719351ddf8e6da04f8000089a2e
asherlie <lieber.asher@gmail.com>
2020-11-12 19:50:05 -0500

    added notes to eval_requests.c, implemented write_mem() and read_mem()

--- eval_requests.c
+++ eval_requests.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "shared.h"
 #include "eval_requests.h"
@@ -14,11 +15,38 @@ void* alloc_mem(struct requester_cont* rc, struct requester* r, int sz, int coun
     return ret;
 }
 
+#if !1
+these two functions need to be reworked to take into account the size of each block
+len should be in terms of block size
+i can maybe make a #define on the nalloc() side to hide this
+#define NALLOC(type, addr, nbytes) nalloc(addr, sizeof(type)*nbytes)
+#endif
+
+/* should len be in terms of bytes or n_elements
+ * for now, like memcpy(), it's done in bytes
+ */
+_Bool write_mem(struct shared_mem* mem, int index, int len, void* src){
+    if(len*mem->sz+ index*mem->sz >= mem->sz * mem->count)return 0;
+    /* memcpy(mem->ptr+index, src, len*mem->sz); */
+    memcpy(mem->ptr+index, src, len);
+    return 1;
+}
+
+/* read_mem() reads len bytes from index and writes it to r->sock */
+_Bool read_mem(struct shared_mem* mem, struct requester* r, int index, int len){
+    if(len+index >= mem->sz * mem->count)return 0;
+    return write(r->sock, mem->ptr+(index), len) == len*mem->sz;
+}
+
 _Bool eval_nalloc_request(struct requester_cont* rc,
                           struct requester* r,
                           struct nalloc_request req){
     if(!r)return 0;
     _Bool ret = 1;
+    /* all requests but READ_MEM will reply with just 4 bytes
+     * READ_MEM responses are the same length as the requested
+     * number of bytes
+     */
     switch(req.request){
         case MEM_ALLOC:
             alloc_mem(rc, r, req.sz, req.count);
